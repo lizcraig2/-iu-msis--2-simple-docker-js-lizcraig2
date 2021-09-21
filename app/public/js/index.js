3ccd783529fc0a2f72b78a1541afae28fa80dd24
lizcraig <84337943+lizcraig@users.noreply.github.com>
2021-09-21 13:47:30 -0400

    finishing touches to random user assignment

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -6,18 +6,14 @@ const someApp = {
       message: "Waiting ...",
     };
   },
+  // Source: https://day.js.org/
   computed: {
     bday(){
       return dayjs(this.result.dob.date).format('DD/MM/YYYY')
     }
   },
 
-  // computed: {
-  //     prettyBirthday() {
-  //         return.this
-  //     }
 
-  // },
   created() {
     //Method 1:
     fetch("https://randomuser.me/api/")
@@ -32,36 +28,9 @@ const someApp = {
   }
   
 }
-
+//Does my console work?
 console.log("hi liz");
-
+//mount app I created
 Vue.createApp(someApp).mount("#SomeApp");
 
-/*  const btn = document.getElementById("btn");
-        btn.addEventListener("click", function() {
-        getPerson(getData);
-        }); 
 
-        function getData(response, callback) {
-            const data = JSON.parse(response);
-          
-        const {
-            name: { first },
-            name: { last },
-            picture: { large },
-            location: { street },
-            phone,
-            email
-          } = data.results[0];
-        
-          callback(first, last, large, street, phone, email);
-        }
-        function showData(first, last, large, street, phone, email) {
-          document.getElementById("first").textContent = first;
-          document.getElementById("last").textContent = last;
-          document.getElementById("street").textContent = street;
-          document.getElementById("phone").textContent = phone;
-          document.getElementById("email").textContent = email;
-          document.getElementById("photo").src = large;
-        }
-        */
